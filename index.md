3496cdc3101d535108238524a0da8d8aeea20907
jims-enemy <161627353+jims-enemy@users.noreply.github.com>
2024-03-19 09:58:03 -0600

    added demo and updated tetris

--- index.md
+++ index.md
@@ -2,6 +2,7 @@
 - [Collision Demo](collision)
 - [Arrays and Objects](circles)
 - [Generative Art Demo](06-art)
+- [Terrain Generation](07-terrain)
 
 # Projects
 - [Interactive Scene](the_answer)
--- tetrisVSbutsp/sketch.js
+++ tetrisVSbutsp/sketch.js
@@ -1,5 +1,10 @@
 let rowLines = 10;
 let columnLines = 20;
+let tetrisBoards = new Map();
+let games = 6;
+for(let board = 0; board < games; board++) {
+  tetrisBoards.set(`tetrisGame${board}`, new Map());
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -7,7 +12,11 @@ function setup() {
 
 function draw() {
   background("black");
-  drawGrid(width/3, 0, width/3 * 2, height);
+  drawGames();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function drawGrid(gridX1, gridY1, gridX2, gridY2) {
@@ -17,5 +26,14 @@ function drawGrid(gridX1, gridY1, gridX2, gridY2) {
   }
   for(let currentColumn = 0; currentColumn < columnLines + 1; currentColumn++) {
     line(gridX1,(gridY2 - gridY1)/columnLines * currentColumn + gridY1, gridX2, (gridY2 - gridY1)/columnLines * currentColumn + gridY1);
+  }
+}
+
+function drawGames() {
+  drawGrid(width/3, 0, width/3 * 2, height);
+  for(let currentGridRow = 0; currentGridRow < Math.ceil((games - 1)/2)) {
+    for(let currentGridColumn = 0; currentGridColumn < Math.floor((games - 1)/2)) {
+      drawGrid()
+    }
   }
 }
