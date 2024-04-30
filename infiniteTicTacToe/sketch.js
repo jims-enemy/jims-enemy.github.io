cb5c5e55be36eb81791de224005cba3e4b3fed78
jims-enemy <161627353+jims-enemy@users.noreply.github.com>
2024-04-30 07:43:05 -0600

    Update sketch.js

--- infiniteTicTacToe/sketch.js
+++ infiniteTicTacToe/sketch.js
@@ -135,19 +135,29 @@ function isThree(thisGrid, checkLine) {
 /** Checks currentGame.grid for a line.
  * If found, sets currentGame.player to the winner and checks if they won the full game. */
 function checkIf3() {
+
+  // Loops through every possible line combination for the current game.
   for (let checkLine = 0; checkLine < 3; checkLine++) {
     if (isThree(currentGame.grid, checkLine)) {
+
+      // Sets the board to be X if they won.
       if (currentPlayer === "Current turn: X") {
         currentGame.player = "X";
       }
+
+      // Otherwise, sets it to O.
       else {
         currentGame.player = "O";
       }
+
       currentGame.drawn = 0;
       currentGame.lastUpdate = timer;
 
+      // Loops through every possible line combination for the full board.
       for (let checkLine = 0; checkLine < 3; checkLine++) {
         if (isThree(megaBoard, checkLine)) {
+          
+          // Sets the time won to be used for the end-of-game delay.
           winnerFound = timer;
         }
       }
@@ -157,28 +167,42 @@ function checkIf3() {
 
 function mouseClicked() {
 
-  // Temporarily the ability to play after the game has ended.
+  // Temporarily the stops the ability to play after the game has ended.
   if (!winnerFound) {
+
+    // Regular play mode.
     if (!pickSquare) {
+
+      // Sets x and y to be the mouse's current location relative to the current board.
       let x = Math.floor(mouseX/(width/(COLUMNS*2))) - COLUMNS;
       let y = Math.floor(mouseY/(height/(ROWS*2))) - ROWS;
 
+      // Ensures you clicked on the board.
       if (x >= 0 && y >= 0) {
+
+        // Places the marker if the spot is empty.
         if (currentGame.grid[y][x] === 0) {
+
+          // Places X and sets the turn to O if it is turn X.
           if (currentPlayer === "Current turn: X") {
             currentGame.grid[y][x] = {player: "X", drawn: 0, lastUpdate: timer};
-            checkIf3();
             currentPlayer = "Current turn: O";
           }
+
+          // Otherwise, place O and set turn to X.
           else {
             currentGame.grid[y][x] = {player: "O", drawn: 0, lastUpdate: timer};
-            checkIf3();
             currentPlayer = "Current turn: X";
           }
 
+          checkIf3();
+
+          // Moves the current board to the same one you clicked on as long as that one can be played on.
           if (megaBoard[y][x].player === 0 && megaBoard[y][x].grid.flatMap(subarray => subarray).some(element => element === 0)) {
             currentGame = megaBoard[y][x];
           }
+
+          // Otherwise, allow the next player to play wherever they want.
           else if (currentGame.player !== 0 || ! currentGame.grid.flatMap(subarray => subarray).some(element => element === 0)) {
             pickSquare = true;
             currentGame = {grid: [[0, 0, 0,], [0, 0, 0,], [0, 0, 0]], R: 255, G: 255, B: 255};
@@ -187,10 +211,14 @@ function mouseClicked() {
       }
     }
 
+    // Free-movement mode, as long as the mouse was clicked within the full board.
     else if (mouseX <= width/2 && mouseY <= height/2) {
+
+      // Sets x and y to be the mouse's current location relative to the full board.
       let x = Math.floor(mouseX/(width/(COLUMNS*2)));
       let y = Math.floor(mouseY/(height/(ROWS*2)));
 
+      // Sets the current board to where the user clicked, as long as they can play in that square.
       if (megaBoard[y][x].player === 0 && megaBoard[y][x].grid.flatMap(subarray => subarray).some(element => element === 0)) {
         currentGame = megaBoard[y][x];
         pickSquare = false;
@@ -209,37 +237,66 @@ function draw() {
 
 }
 
-function drawBoards(){
-  for (let currentColumn = 0; currentColumn < COLUMNS; currentColumn++) {
-    for (let currentRow = 0; currentRow < ROWS; currentRow++) {
-      stroke(megaBoard[currentColumn][currentRow].R, megaBoard[currentColumn][currentRow].G, megaBoard[currentColumn][currentRow].B);
-      drawGame(0 + width/6 * currentRow, 0 + height/6 * currentColumn, width/6 * (currentRow + 1), height/6 * (currentColumn + 1));
-      drawPlayers(width/6 * currentRow, height/6 * currentColumn,
-        width/6 * (currentRow + 1), height/6 * (currentColumn + 1),
-        megaBoard[currentColumn][currentRow].grid);
-    }
-  }
-
-  stroke(currentGame.R, currentGame.G, currentGame.B);
-  drawGame(width/2, height/2, width, height);
-  drawPlayers(width/2, height/2, width, height, currentGame.grid);
-
-  stroke("white");
-  drawGame(0, 0, width/2, height/2);
-  drawPlayers(0, 0, width/2, height/2, megaBoard);
-}
-
-
+/** Draws a grid.
+ * @param {number} x1 - The first x-coordinate of the grid.
+ * @param {number} y1 - The first y-coordinate of the grid.
+ * @param {number} x2 - The second x-coordinate of the grid.
+ * @param {number} y2 - The second y-coordinate of the grid. */
 function drawGame(x1, y1, x2, y2) {
+
   // Draws the columns.
   for (let currentColumn = 0; currentColumn <= COLUMNS; currentColumn++) {
     line((x2 - x1)/COLUMNS * currentColumn + x1, y1, (x2 - x1)/COLUMNS * currentColumn + x1, y2);
   }
-
+  
   // Draws the rows.
   for (let currentRow = 0; currentRow <= ROWS; currentRow++) {
     line(x1,(y2 - y1)/ROWS * currentRow + y1, x2, (y2 - y1)/ROWS * currentRow + y1);
   }
+}
+
+/** Updates the current square's percentage drawn and when it was last updated.
+ * @param {object} thisSquare - The current square to be drawn. */
+function updateTimer(thisSquare) {
+
+  // Updates the percentage drawn if enough time has passed since it was last updated.
+  if (timer >= drawSpeed/100 + thisSquare.lastUpdate &&
+            thisSquare.drawn < 100) {
+    thisSquare.drawn += (timer - thisSquare.lastUpdate)/(drawSpeed/100);
+
+    // Caps it at 100% drawn.
+    if (thisSquare.drawn >= 100) {
+      thisSquare.drawn = 100;
+    }
+
+    // Sets the last updated time to the current time.
+    thisSquare.lastUpdate = timer;
+  }
+}
+
+/** Draws an X.
+ * @param {number} squareX1 - The first x-coordinate of the square.
+ * @param {number} squareY1 - The first y-coordinate of the square.
+ * @param {number} squareX2 - The second x-coordinate of the square.
+ * @param {number} squareY2 - The second y-coordinate of the square.
+ * @param {object} thisSquare - The square that the X is being drawn in.
+ * @param {number} yDistance - squareY2 minus squareY1.
+ * @param {number} speedUp - A value that increases exponentially the more percentage drawn is.
+ * @param {number} slowDown - A value that increases logarithmically the more percentage drawn is. */
+function drawX(squareX1, squareY1, squareX2, squareY2, thisSquare, yDistance, speedUp, slowDown) {
+
+  // When drawing the first line, speed up e
+  if (thisSquare.drawn <= 50) {
+    line(squareX1, squareY1, squareX1 + (squareX2 - squareX1) * speedUp,
+      squareY1 + yDistance * speedUp);
+  }
+  else {
+    line(squareX1, squareY1, squareX2, squareY2);
+    line(squareX2, squareY1, squareX2 + (squareX1 - squareX2) * slowDown,
+      squareY1 + yDistance * slowDown);
+  }
+
+  updateTimer(thisSquare);
 }
 
 function drawPlayers(x1, y1, x2, y2, game) {
@@ -267,21 +324,35 @@ function drawPlayers(x1, y1, x2, y2, game) {
     }
   }
 }
+/** Draws every game. */
+function drawBoards(){
 
-function drawX(squareX1, squareY1, squareX2, squareY2, thisSquare, yDistance, speedUp, slowDown) {
+  // Loops through every square on the mega board.
+  for (let currentColumn = 0; currentColumn < COLUMNS; currentColumn++) {
+    for (let currentRow = 0; currentRow < ROWS; currentRow++) {
 
-  if (thisSquare.drawn <= 50) {
-    line(squareX1, squareY1, squareX1 + (squareX2 - squareX1) * speedUp,
-      squareY1 + yDistance * speedUp);
+      // Sets the color to the board's RGB values.
+      stroke(megaBoard[currentColumn][currentRow].R, megaBoard[currentColumn][currentRow].G, megaBoard[currentColumn][currentRow].B);
+
+      drawGame(0 + width/6 * currentRow, 0 + height/6 * currentColumn, width/6 * (currentRow + 1), height/6 * (currentColumn + 1));
+      drawPlayers(width/6 * currentRow, height/6 * currentColumn,
+        width/6 * (currentRow + 1), height/6 * (currentColumn + 1),
+        megaBoard[currentColumn][currentRow].grid);
+    }
   }
-  else {
-    line(squareX1, squareY1, squareX2, squareY2);
-    line(squareX2, squareY1, squareX2 + (squareX1 - squareX2) * slowDown,
-      squareY1 + yDistance * slowDown);
-  }
 
-  updateTimer(thisSquare);
+  stroke(currentGame.R, currentGame.G, currentGame.B);
+  drawGame(width/2, height/2, width, height);
+  drawPlayers(width/2, height/2, width, height, currentGame.grid);
+
+  stroke("white");
+  drawGame(0, 0, width/2, height/2);
+  drawPlayers(0, 0, width/2, height/2, megaBoard);
 }
+
+
+
+
 
 
 function drawO(squareX1, squareY1, squareX2, thisSquare, yDistance, speedUp, slowDown) {
@@ -298,16 +369,6 @@ function drawO(squareX1, squareY1, squareX2, thisSquare, yDistance, speedUp, slo
 
 }
 
-function updateTimer(thisSquare) {
-  if (timer >= drawSpeed/100 + thisSquare.lastUpdate &&
-            thisSquare.drawn < 100) {
-    thisSquare.drawn += (timer - thisSquare.lastUpdate)/(drawSpeed/100);
-    if (thisSquare.drawn >= 100) {
-      thisSquare.drawn = 100;
-    }
-    thisSquare.lastUpdate = timer;
-  }
-}
 
 function drawText() {
   let biggerWinner;
