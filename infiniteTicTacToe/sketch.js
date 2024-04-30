e1b2ba3c7064fd01e3b15b8153f6db1a8d0aae20
jims-enemy <161627353+jims-enemy@users.noreply.github.com>
2024-04-30 02:12:55 -0600

    Update sketch.js

--- infiniteTicTacToe/sketch.js
+++ infiniteTicTacToe/sketch.js
@@ -1,30 +1,163 @@
 let currentGame;
-let drawSpeed = 1000;
-let timer;
 let megaBoard;
-let currentPlayer = "Current turn: X";
 let pickSquare;
-let winnerFound;
-let xWins = 0;
-let oWins = 0;
 let tieDelay;
+let timer;
+let winnerFound;
 
+// CurrentPlayer MUST be either set to "Current turn: X" or "Current turn: O".
+let currentPlayer = "Current turn: X";
+
+// Determines how long it takes to complete the animation of either an X or O, in milliseconds.
+let drawSpeed = 1000;
+
+// Sets starting win values.
+let oWins = 0;
+let xWins = 0;
+
+const ACCELERATION = 1.0813826568003;
 const COLUMNS = 3;
 const ROWS = 3;
-const ACCELERATION = 1.0813826568003;
+
+/** Sets the square's color based on currentColumn.
+* 
+* @param {number} currentColumn - The index of the current column.
+* @returns {number[]} - An array containing the RGB color values.
+*/
+function setSquareColor(currentColumn) {
+
+  // Sets the first color to 255 after the first loop.
+  let currentColor = 510;
+
+  // Must be negative so it is set to 0 on the first loop.
+  let otherColor = -127.5;
+  let otherOtherColor = -127.5;
+
+  // Loops through each column, applying the color rules.
+  for (let cycle = 0; cycle <= currentColumn; cycle++) {
+    currentColor = currentColor/2;
+    otherColor += currentColor/2;
+    otherOtherColor += currentColor/2;
+  }
+
+  // Returns the result as an array.
+  return [currentColor, otherColor, otherOtherColor];
+}
+
+/** Sets megaBoard to its default values and colors. */
+function resetMegaBoard() {
+
+  // Empty out whatever is in there.
+  megaBoard = [[], [], []];
+
+  // Creates a loop to set all of the colors to their default values.
+  for (let currentColumn = 0; currentColumn < COLUMNS; currentColumn++) {
+    for (let currentRow = 0; currentRow < ROWS; currentRow++) {
+      let thisR;
+      let thisB;
+      let thisG;
+      
+      // The "red" row.
+      if (currentRow === 0) {
+        [thisR, thisG, thisB] = setSquareColor(currentColumn);
+      }
+  
+      // The "green" row.
+      else if (currentRow === 1) {
+        [thisG, thisR, thisB] = setSquareColor(currentColumn);
+      }
+  
+      // The "blue" row.
+      else {
+        [thisB, thisR, thisG] = setSquareColor(currentColumn);
+      }
+  
+      // Pushes the new empty colored game onto the board.
+      megaBoard[currentColumn][currentRow] = {grid: [[0, 0, 0,], [0, 0, 0,], [0, 0, 0]], R: thisR,
+        G: thisG, B: thisB, player: 0};
+    }
+  }
+}
+
+/** Resets the game to its default values.
+ * 
+ * @param {boolean} updateWins - If true, increments the winner's win counter by one.
+*/
+function resetGame(updateWins) {
+
+  // Increments the win count if needed.
+  if (updateWins) {
+    if (currentPlayer === "Current turn: X") {
+      oWins++;
+    }
+    else {
+      xWins++;
+    }
+  }
+
+  // Empties currentGame and sets the remaining values back to where they were at the start.
+  currentGame = {grid: [[0, 0, 0,], [0, 0, 0,], [0, 0, 0]], R: 255, G: 255, B: 255};
+  resetMegaBoard();
+  pickSquare = true;
+  winnerFound = 0;
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noFill();
-  resetGame(false);
-
+  resetGame(false);  
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+/** Determines whether or not someone has three in a line, checking along checkLine on thisGrid.
+ * 
+ * @param {Array} thisGrid - The 2D array you want to check.
+ * @param {number} checkLine - Which horizontal and vertical line you want to check.
+ * @returns {boolean} - Returns if there is a line or not.
+*/
+function isThree(thisGrid, checkLine) {
+  return thisGrid[checkLine][0].player === thisGrid[checkLine][1].player &&
+    thisGrid[checkLine][1].player === thisGrid[checkLine][2].player &&
+    (thisGrid[checkLine][2].player === "X" || thisGrid[checkLine][2].player === "O") ||
+    thisGrid[0][checkLine].player === thisGrid[1][checkLine].player &&
+    thisGrid[1][checkLine].player === thisGrid[2][checkLine].player &&
+    (thisGrid[2][checkLine].player === "X" || thisGrid[2][checkLine].player === "O") ||
+    (thisGrid[0][0].player === thisGrid[1][1].player &&
+    thisGrid[1][1].player === thisGrid[2][2].player ||
+    thisGrid[0][2].player === thisGrid[1][1].player &&
+    thisGrid[1][1].player === thisGrid[2][0].player) &&
+    (thisGrid[1][1].player === "X" || thisGrid[1][1].player === "O");
+}
+
+/** Checks currentGame.grid for a line.
+ * If found, sets currentGame.player to the winner and checks if they won the full game. */
+function checkIf3() {
+  for (let checkLine = 0; checkLine < 3; checkLine++) {
+    if (isThree(currentGame.grid, checkLine)) {
+      if (currentPlayer === "Current turn: X") {
+        currentGame.player = "X";
+      }
+      else {
+        currentGame.player = "O";
+      }
+      currentGame.drawn = 0;
+      currentGame.lastUpdate = timer;
+
+      for (let checkLine = 0; checkLine < 3; checkLine++) {
+        if (isThree(megaBoard, checkLine)) {
+          winnerFound = timer;
+        }
+      }
+    }
+  }
+}
+
 function mouseClicked() {
+
+  // Temporarily the ability to play after the game has ended.
   if (!winnerFound) {
     if (!pickSquare) {
       let x = Math.floor(mouseX/(width/(COLUMNS*2))) - COLUMNS;
@@ -72,7 +205,8 @@ function draw() {
 
   timer = millis();
   drawBoards();
-  checkForTie();
+  checkForEnd();
+
 }
 
 function drawBoards(){
@@ -170,54 +304,8 @@ function updateTimer(thisSquare) {
     thisSquare.drawn += (timer - thisSquare.lastUpdate)/(drawSpeed/100);
     if (thisSquare.drawn >= 100) {
       thisSquare.drawn = 100;
-      if (winnerFound > 0 && timer > winnerFound + drawSpeed * 2) {
-        resetGame(true);
-      }
     }
     thisSquare.lastUpdate = timer;
-  }
-}
-
-function checkIf3() {
-  for (let checkLine = 0; checkLine < 3; checkLine++) {
-    if (currentGame.grid[checkLine][0].player === currentGame.grid[checkLine][1].player &&
-    currentGame.grid[checkLine][1].player === currentGame.grid[checkLine][2].player &&
-    (currentGame.grid[checkLine][2].player === "X" || currentGame.grid[checkLine][2].player === "O") ||
-    currentGame.grid[0][checkLine].player === currentGame.grid[1][checkLine].player &&
-    currentGame.grid[1][checkLine].player === currentGame.grid[2][checkLine].player &&
-    (currentGame.grid[2][checkLine].player === "X" || currentGame.grid[2][checkLine].player === "O") ||
-    (currentGame.grid[0][0].player === currentGame.grid[1][1].player &&
-    currentGame.grid[1][1].player === currentGame.grid[2][2].player ||
-    currentGame.grid[0][2].player === currentGame.grid[1][1].player &&
-    currentGame.grid[1][1].player === currentGame.grid[2][0].player) &&
-    (currentGame.grid[1][1].player === "X" || currentGame.grid[1][1].player === "O")) {
-      if (currentPlayer === "Current turn: X") {
-        currentGame.player = "X";
-        currentGame.drawn = 0;
-        currentGame.lastUpdate = timer;
-      }
-      else {
-        currentGame.player = "O";
-        currentGame.drawn = 0;
-        currentGame.lastUpdate = timer;
-      }
-
-      for (let checkLine = 0; checkLine < 3; checkLine++) {
-        if (megaBoard[checkLine][0].player === megaBoard[checkLine][1].player &&
-        megaBoard[checkLine][1].player === megaBoard[checkLine][2].player &&
-        (megaBoard[checkLine][2].player === "X" || megaBoard[checkLine][2].player === "O") ||
-        megaBoard[0][checkLine].player === megaBoard[1][checkLine].player &&
-        megaBoard[1][checkLine].player === megaBoard[2][checkLine].player &&
-        (megaBoard[2][checkLine].player === "X" || megaBoard[2][checkLine].player === "O") ||
-        (megaBoard[0][0].player === megaBoard[1][1].player &&
-        megaBoard[1][1].player === megaBoard[2][2].player ||
-        megaBoard[0][2].player === megaBoard[1][1].player &&
-        megaBoard[1][1].player === megaBoard[2][0].player) &&
-        (megaBoard[1][1].player === "X" || megaBoard[1][1].player === "O")) {
-          winnerFound = timer;
-        }
-      }
-    }
   }
 }
 
@@ -258,64 +346,11 @@ function drawText() {
 
 }
 
-function resetGame(updateWins) {
-  if (updateWins) {
-    if (currentPlayer === "Current turn: X") {
-      oWins++;
-    }
-    else {
-      xWins++;
-    }
+
+function checkForEnd() {
+  if (winnerFound > 0 && timer > winnerFound + drawSpeed * 2) {
+    resetGame(true);
   }
-  currentGame = {grid: [[0, 0, 0,], [0, 0, 0,], [0, 0, 0]], R: 255, G: 255, B: 255};
-  megaBoard = [[], [], []];
-
-  for (let currentColumn = 0; currentColumn < COLUMNS; currentColumn++) {
-    for (let currentRow = 0; currentRow < ROWS; currentRow++) {
-      let thisR = -127.5;
-      let thisG = -127.5;
-      let thisB = -127.5;
-      
-      if (currentRow === 0) {
-        thisR = 510;
-  
-        for (let cycle = 0; cycle <= currentColumn; cycle++) {
-          thisR = thisR/2;
-          thisG += thisR/2;
-          thisB += thisR/2;
-        }
-      }
-  
-      else if (currentRow === 1) {
-        thisG = 510;
-  
-        for (let cycle = 0; cycle <= currentColumn; cycle++) {
-          thisG = thisG/2;
-          thisR += thisG/2;
-          thisB += thisG/2;
-        }
-      }
-  
-      else {
-        thisB = 510;
-  
-        for (let cycle = 0; cycle <= currentColumn; cycle++) {
-          thisB = thisB/2;
-          thisR += thisB/2;
-          thisG += thisB/2;
-        }
-      }
-  
-      megaBoard[currentColumn][currentRow] = {grid: [[0, 0, 0,], [0, 0, 0,], [0, 0, 0]], R: thisR,
-        G: thisG, B: thisB, player: 0};
-    }
-  }
-
-  pickSquare = true;
-  winnerFound = 0;
-}
-
-function checkForTie() {
 
   for (let gameY of megaBoard) {
     for (let gameX of gameY) {
